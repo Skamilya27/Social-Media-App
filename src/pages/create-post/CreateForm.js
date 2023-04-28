6528d1437b32a73c103885a236f752a4e85a69af
Suman <sumankamilya27@gmail.com>
2023-04-28 11:57:31 +0530

    sending data to database

--- src/pages/create-post/CreateForm.js
+++ src/pages/create-post/CreateForm.js
@@ -2,8 +2,15 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 export const CreateForm = () => {
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+
   const schema = yup.object().shape({
     title: yup.string().required("You must add a title"),
     description: yup.string().required("You must add a description"),
@@ -17,8 +24,19 @@ export const CreateForm = () => {
     resolver: yupResolver(schema),
   });
 
-  const onCreatePost = (data) => {
-    console.log(data);
+  const postsRef = collection(db, "posts"); 
+
+  const onCreatePost = async (data) => {
+    await addDoc(postsRef, {
+        // title: data.title,
+        // description: data.description,
+        //we can also do
+        ...data,
+        username: user?.displayName,
+        userId: user?.uid,
+    });
+
+    navigate("/");
   };
 
   return (
@@ -27,7 +45,7 @@ export const CreateForm = () => {
       <p style={{ color: "red" }}>{errors.title?.message}</p>
       <textarea placeholder="Description..." {...register("description")} />
       <p style={{ color: "red" }}>{errors.description?.message}</p>
-      <input type="submit" />
+      <input type="submit" className="submitForm"/>
     </form>
   );
 };
